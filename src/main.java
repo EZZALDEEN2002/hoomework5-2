c70652b796c89a4e792d891ee7d5a303669f52c0
EZZALDEEN SHARAHA <150586726+EZZALDEEN2002@users.noreply.github.com>
2023-11-12 19:49:30 +0300

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args) {
+        Circle[] circles = {
+                new Circle(5),
+                new Circle(10),
+                new Circle(15),
+        };
+
+        for (Circle circle : circles) {
+            double area = circle.computeArea();
+            System.out.println("Area: " + area);
+        }
+    }
+}
+
+
